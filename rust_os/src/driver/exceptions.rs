8026d702d0448827e3c132cc122e85a806f31b06
theRealProHacker <77074862+theRealProHacker@users.noreply.github.com>
2023-01-16 11:33:58 +0100

    debug in src1handler

--- rust_os/src/driver/exceptions.rs
+++ rust_os/src/driver/exceptions.rs
@@ -209,9 +209,11 @@ extern "aapcs" fn src1_handler(regs: &mut Registers) {
         }
     } else if dbgu.status.read() & serial::RXRDY != 0 {
         let char = dbgu.read() as u32;
+        println!("Read char {}", char as u8 as char);
         for mut thread in threads.array.iter_mut().filter_map(|&mut x| x) {
             thread.state = match thread.state {
                 WaitingForChar => {
+                    println!("Found {thread:?}");
                     thread.regs.r0 = char;
                     Ready
                 }
